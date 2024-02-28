24f8686985dba2d75843598c1be2cdd53e77cd59
nextbowl <minnekanti444@gmail.com>
2024-02-28 21:20:32 +0000

    cluster files added

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "google_storage_bucket" {
+  type        = string
+  default     = "lavanya1234"
+  description = "Project ID for billing"
+}
+variable "location" {
+  type        = string
+  default     = "us-east-1a"
+  description = "Group ID of the Group"
+}
